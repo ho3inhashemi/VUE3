af0f9101d4f3202441b63f6c3823cfe2755ab9b2
ho3inhashemi <ho3inhashemi@gmail.com>
2022-01-13 19:37:46 +0330

    dynamic deleting li item by clicking with computed property

--- app.js
+++ app.js
@@ -7,31 +7,36 @@ const app = Vue.createApp({
             url:"http://www.google.com",
             showBooks: true,
             books:[
-                {title: "Bof koor", author: "Sadegh Hedayat", image:"images/boofkoor.jpg" , isFave: false},
-                {title: "Sag velgard", author: "Sadegh Hedayat", image:"images/kelidar.jpeg", isFave: false},
-                {title: "Kelidar", author: "Mahmud DowlatAbadi", image:"images/sagvelgard.jpg", isFave: false},
+                {title: "Bof koor", author: "Sadegh Hedayat", image:"images/boofkoor.jpg" , isFave: true},
+                {title: "Sag velgard", author: "Sadegh Hedayat", image:"images/kelidar.jpeg", isFave: true},
+                {title: "Kelidar", author: "Mahmud DowlatAbadi", image:"images/sagvelgard.jpg", isFave: true},
             ]
         }
     },
     methods: {
         // changeTitle(title){
-            // this.title = title;
-            changeShowBooks(){
-                 this.showBooks = !this.showBooks
-                },
+        // this.title = title;
+        changeShowBooks(){
+            this.showBooks = !this.showBooks
+            },
 
-            handleEvent(e){
-                console.log(e)
-                },
+        handleEvent(e){
+            console.log(e)
+            },
 
-            handleMouseMove(e){
-                this.x = e.offsetX
-                this.y = e.offsetY
-                },
+        handleMouseMove(e){
+            this.x = e.offsetX
+            this.y = e.offsetY
+            },
 
-            changeFav(book){
-                book.isFave = !book.isFave
-            }
+        changeFav(book){
+            book.isFave = !book.isFave
+            },
+    },
+    computed: {
+        filteredBooks(){
+            return this.books.filter(book => book.isFave)
+        }
     }
 })
 
